11fe6d20f074d20cb2ae616a737fbfe47fea197c
ronak-soni <ronakvsoni@gmail.com>
2020-08-31 06:50:28 -0700

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  addingEventListener();
+});
+
 function addingEventListener() {
   const input = document.getElementById("input");
   input.addEventListener("click", function (event) {
+    console.log("hii");
     alert("I was clicked!");
   });
 }
